bc48cd994cb99d61927a2f85eaaea441cf7b4a86
Kevin <101673253+keaguirre@users.noreply.github.com>
2024-04-14 05:26:38 -0400

    update for ev 1, script done for gh actions

--- scriptBash.sh
+++ scriptBash.sh
@@ -1,50 +1,277 @@
 #!/bin/bash
-# AzureCLI Bash
-# URL Material https://learn.microsoft.com/en-us/azure/virtual-machines/windows/quick-create-cli
 
-# Set variables 4 resource group
-resource_group="actividad-2"
-location="eastus"
+# Declaración de variables
+resource_group_status=false
+vm_status=false
+webserver_status=false
+port_opened=false
+db_server=false
+sql_status=false
+firewall_conf=false
+ip_name="public"
+virtualnet_status=false
+public_ip_status=false
+nsg_rules_status=false
+ip_status=false
+role_assigned=false
+resource_group="$INPUT_RESOURCE_GROUP"
+location="$INPUT_LOCATION"
+vm_name="$INPUT_VM_NAME"
+username="$INPUT_USERNAME"
+password="$INPUT_PASSWORD"
+nsg_name="$INPUT_NSG_NAME"
+nic_name="$INPUT_NIC_NAME"
+subnet_name="$INPUT_SUBNET_NAME"
+vnet_name="$INPUT_VNET_NAME"
+db_name="$INPUT_DB_NAME"
+role="$INPUT_ROLE"
+sub_id="$INPUT_SUB_ID"
 
-# Vars for Create VM
-vm_name="vm-actividad-2"
-username="$AZURE_VM_USERNAME"
-password="$AZURE_VM_PASSWORD"
+# Función para manejar errores
+handle_error() {
+    echo "Error: $1" >&2
+    exit 1
+}
 
-# Resource Group
-az group create --name $resource_group --location $location
+# Función para crear el grupo de recursos
+create_resource_group() {
+    echo "Creando grupo de recursos..."
+    if az group create --name "$resource_group" --location "$location" --tags aay5121=grupo3; then
+        resource_group_status=true
+    else
+        handle_error "No se pudo crear el grupo de recursos."
+    fi
+}
 
-# Create VM retrieves a json obj and Take a note your own publicIpAddress in the output when you create your VM. 
-az vm create --resource-group $resource_group --name $vm_name --image MicrosoftWindowsServer:WindowsServer:2022-datacenter-azure-edition:latest --public-ip-sku Standard --admin-username $username --admin-password $password
+create_virtualnet_subnet(){
+    echo "Creando Redvirtual y Subnet..."
+    if az network vnet create --resource-group "$resource_group" --name "$vnet_name" --address-prefix "192.168.0.0/16" --subnet-name "$subnet_name" --subnet-prefix "192.168.10.0/24"; then
+        virtualnet_status=true
+    else
+        handle_error "No se pudo crear la red virtual o la subred"
+    fi
+}
 
-# Install SII
-az vm run-command invoke -g $resource_group -n $vm_name --command-id RunPowerShellScript --scripts "Install-WindowsFeature -name Web-Server -IncludeManagementTools"
+# IP Publica
+ip_create(){
+    echo "Creando IP Publica..."
+    if az network public-ip create --resource-group "$resource_group" --name "$ip_name" --sku Standard --allocation-method Static; then
+        public_ip_status=true
+    else
+        handle_error "No se pudo crear la IP Publica"
+    fi
+}
 
-# Open webserver port
-az vm open-port --port 80 --resource-group $resource_group --name $vm_name
+create_nsg(){
+    echo "Creando Network Security Group..."
+    if az network nsg create --resource-group "$resource_group" --name "$nsg_name"; then
+        nsg_status=true
+    else
+        handle_error "No se pudo crear el Network Security Group"
+    fi
+}
 
-# Show public ip
-ip_publica=$(az vm show --resource-group $resource_group --name $vm_name --show-details | jq -r '.publicIps')
+nsg_rules(){
+    echo "Creando reglas RDP y HTTP con origen abierto...3389"
+    if az network nsg rule create --resource-group "$resource_group" --nsg-name "$nsg_name" --name RDPAccess --priority 1000 --protocol Tcp --destination-port-range 3389 --access Allow --direction Inbound --source-address-prefix "0.0.0.0/0"; then
+        echo "NSG RDP Rule created"
+        if az network nsg rule create --resource-group "$resource_group" --nsg-name "$nsg_name" --name HTTPAccess --priority 1010 --protocol Tcp --destination-port-range 80 --access Allow --direction Inbound --source-address-prefix "0.0.0.0/0"; then
+            echo "NSG HTTP Rule created"
+            nsg_rules_status=true
+        else
+            handle_error "No se pudo crear la regla HHTP para el NSG"
+        fi
+    else
+        handle_error "No se pudo crear la reglas RDP para el NSG"
+    fi
 
-# Check with curl public ip, if retrieves an html response then everything is ok
-response=$(curl -sSL "http://$ip_publica")
-echo response: $response
+}
 
-if [ -n "$response" ]; then
-  if [[ $(awk '/<!DOCTYPE html|<!doctype html/ {print}' <<< "$response") ]]; then
-    echo "web ok"
-    # Eliminar grupo de recursos creado
-    az group delete --name $resource_group
-  else
-    echo "La respuesta no es una página HTML o hay un error en la respuesta."
-    az group delete --name $resource_group
-  fi
-else
-  echo "La respuesta es nula."
-  az group delete --name $resource_group
+check_ip(){
+    while :; do
+        ip_address=$(az network public-ip show --name "$ip_name" --resource-group "$resource_group" --query ipAddress --output tsv 2>/dev/null)
+        if [[ -n "$ip_address" && "$ip_address" != "null" ]]; then
+            sleep 2
+            echo "La IP pública encontrada es: $ip_address"
+            ip_status=true
+            break # Salir del bucle cuando la IP pública esté disponible
+        else
+            echo "Aún no... comprobando nuevamente"
+            sleep 10 # Esperar 10 segundos antes de intentarlo de nuevo
+        fi
+    done
+}
+
+create_nic(){
+    if az network nic create --resource-group "$resource_group" --name "$nic_name" --vnet-name "$vnet_name" --subnet "$subnet_name" --network-security-group "$nsg_name" --public-ip-address "$ip_name"; then
+        nic_status=true
+    else
+        handle_error "Error al crear NIC (Network Interface Card)..."
+    fi
+}
+
+# Función para crear la máquina virtual
+create_vm() {
+    echo "Creando máquina virtual..."
+    if az vm create --resource-group "$resource_group" --name "$vm_name" --image MicrosoftWindowsServer:WindowsServer:2019-datacenter:latest --public-ip-sku Standard --admin-username "$username" --admin-password "$password" --tags aay5121=grupo3 --nics "$nic_name"; then
+        vm_status=true
+    else
+        handle_error "No se pudo crear la máquina virtual."
+    fi
+}
+
+# Función para instalar el servidor web en la máquina virtual
+install_web_server() {
+    echo "Instalando servidor web en la máquina virtual..."
+    if az vm run-command invoke -g "$resource_group" -n "$vm_name" --command-id RunPowerShellScript --scripts "Install-WindowsFeature -name Web-Server -IncludeManagementTools"; then
+        webserver_install_status=true
+    else
+        handle_error "No se pudo instalar el servidor web."
+    fi
+}
+
+# Función para abrir el puerto 80 en la máquina virtual
+open_port_80() {
+    echo "Abriendo puerto 80 en la máquina virtual..."
+    if az vm open-port --port 80 --resource-group "$resource_group" --name "$vm_name"; then
+        port_opened=true
+    else
+        handle_error "No se pudo abrir el puerto 80."
+    fi
+}
+
+# Function to check web server status
+check_web_server_status() {
+    # Show public ip
+    ip_publica=$(az network public-ip show --resource-group "$resource_group" --name "$ip_name" | jq -r '.ipAddress')
+    echo "Public IP check_web_server_status: $ip_publica"
+    echo "Public IP created before: $ip_address"
+    # Check with curl public ip, if retrieves an html response then everything is ok
+    response=$(curl -sSL "http://$ip_publica")
+    echo response: "$response"
+
+    if [ -n "$response" ]; then
+        if [[ $(awk '/<!DOCTYPE html|<!doctype html/ {print}' <<< "$response") ]]; then
+            echo "Web server is running."
+            webserver_status=true
+        else
+            echo "The response is not an HTML page or there is an error in the response."
+            # az group delete --name $resource_group
+        fi
+    else
+        echo "The response is null."
+        # az group delete --name $resource_group
+    fi
+}
+
+# Create DB
+create_sql_server() {
+    echo "Creando servidor SQL..."
+    if az sql server create --name "$vm_name" --resource-group "$resource_group" --location "$location" --admin-user "$username" --admin-password "$password"; then
+        db_server=true
+    else
+        handle_error "No se pudo crear el servidor SQL."
+    fi
+}
+
+# Función para realizar la limpieza de recursos
+create_firewall_rules() {
+    echo "Configurando firewall..."
+    if az sql server firewall-rule create --resource-group "$resource_group" --server "$vm_name" --name AllowYourIp --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0; then
+        firewall_conf=true
+    else
+        handle_error "No se pudo eliminar el grupo de recursos."
+    fi
+}
+
+create_db() {
+    echo "Creando $vm_name SQL"
+    if az sql db create --resource-group "$resource_group" --server "$vm_name" --name "$db_name" --sample-name AdventureWorksLT --edition GeneralPurpose --compute-model Serverless --family Gen5 --capacity 2; then
+        sql_status=true
+
+        # Comando sqlcmd para ejecutar una consulta de prueba
+        echo "Ejecutando consulta de prueba en la base de datos..."
+        if sqlcmd -S "$vm_name.database.windows.net" -d "$db_name" -U "$username" -P "$password" -Q "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName FROM SalesLT.ProductCategory pc JOIN SalesLT.Product p ON pc.productcategoryid = p.productcategoryid;"; then
+            echo "Consulta ejecutada exitosamente."
+        else
+            handle_error "No se pudo ejecutar la consulta de prueba."
+        fi
+
+    else
+        handle_error "No se pudo crear el servidor SQL."
+    fi
+}
+
+# Función para asignar un rol a un usuario en el grupo de recursos
+assign_role_to_user() {
+    echo "Asignando rol al usuario..."
+    user_email="chr.cabrera@duocuc.cl"
+    role="Contributor"
+    if az role assignment create --assignee "$user_email" --role "$role" --scope "/subscriptions/$sub_id/resourceGroups/$resource_group"; then
+        echo "Rol asignado con éxito."
+        role_assigned=true
+    else
+        handle_error "No se pudo asignar el rol al usuario."
+    fi
+}
+
+# Función para realizar la limpieza de recursos
+cleanup_resources() {
+    echo "Limpiando recursos..."
+    if az group delete --name "$resource_group" --yes; then
+        cleanup_done=true
+    else
+        handle_error "No se pudo eliminar el grupo de recursos."
+    fi
+}
+
+# Manejar errores y ejecutar funciones en orden
+create_resource_group
+if $resource_group_status; then
+    create_virtualnet_subnet
+fi    
+if $virtualnet_status; then
+    ip_create
+fi    
+if $public_ip_status; then
+    create_nsg
+fi    
+if $nsg_status; then
+    nsg_rules
 fi
-
-# CleanUP Resources
-# az group delete --name actividad-2
-echo "Script ok, eliminando los recursos usados"
-az group delete --name $resource_group
+if $nsg_rules_status; then
+    check_ip
+fi
+if $ip_status; then
+    create_nic
+fi
+if $nic_status; then
+    create_vm
+fi
+if $vm_status; then
+    install_web_server
+fi
+if $webserver_install_status; then
+    open_port_80
+fi
+if $port_opened; then
+    check_web_server_status
+fi
+if $webserver_status; then
+    create_sql_server
+fi
+if $db_server; then
+    create_firewall_rules
+fi
+if $firewall_conf; then
+    create_db
+fi
+if $sql_status; then
+    assign_role_to_user
+fi
+if $role_assigned; then
+    # cleanup_resources
+    echo "Script terminado"
+fi
+# if $cleanup_done; then
+# fi
